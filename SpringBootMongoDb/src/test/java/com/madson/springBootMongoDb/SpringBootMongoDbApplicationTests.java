7e941f5a1c2dcf5c6714f44d8dae27c5154b7993
Madson Cardoso <madson91@gmail.com>
2021-10-11 10:09:15 -0300

    primeiro commit - iniciando projeto.

---
+++ SpringBootMongoDb/src/test/java/com/madson/springBootMongoDb/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madson.springBootMongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
